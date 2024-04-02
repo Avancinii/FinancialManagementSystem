e1d2eee2c7fd8d784329f01187b04dcc03bc78d6
Avancinii <gustavo_avancini100997@outlook.com>
2024-04-02 20:10:41 -0300

    Adicionando Projeto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Project.Models;
+using System.Diagnostics;
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        [HttpGet]
+        public IActionResult Index(HomeModel form)
+        {
+            LoadData();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public void LoadData()
+        {
+            HomeModel objHome = new HomeModel();
+            ViewBag.List = objHome.GetAll();
+        }
+    }
+}
---
+++ Models/HomeModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Project.Models
+{
+    public class HomeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<HomeModel> GetAll()
+        {
+            HomeModel model;
+            List<HomeModel> list = new List<HomeModel>();
+
+            model = new HomeModel();
+            model.Id = 1;
+            model.Name = "Gustavo Avancini";
+            list.Add(model);
+
+            model = new HomeModel();
+            model.Id = 2;
+            model.Name = "Alexandre Fialho";
+            list.Add(model);
+
+            return list;
+        }
+    }
+}
