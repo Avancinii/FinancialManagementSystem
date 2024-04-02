64ef2e98d250b0f5fc470a01ff48ee2e89efc8df
Avancinii <gustavo_avancini100997@outlook.com>
2024-04-02 20:53:22 -0300

    Adicionando classe de acesso a dados MySql

---
+++ Util/DAL.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Project.Util
+{
+    public class DAL
+    {
+        private static string server = "localhost";
+        private static string database = "financeiro";
+        private static string user = "root";
+        private static string password = "";
+        private string connectionString = $"Server={server};Database={database};Uid={user};Pwd={password}";
+        private MySqlConnection connection;
+
+        public DAL()
+        {
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+        }
+        public DataTable RetDataTable(string sql)
+        {
+            DataTable dt = new DataTable();
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            adapter.Fill(dt);
+            return dt;
+        }
+        public void ExecutarComandoSQL(string sql)
+        {
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            command.ExecuteNonQuery();
+        }
+    }
+}
